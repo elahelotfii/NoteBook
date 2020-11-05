20af3d96f3736fbc1151c92d13d0599b486e7ac3
elahelotfii <elahe.lotfi1991@gmail.com>
2020-11-04 16:27:10 -0800

    second changes

--- app/src/main/java/com/example/elilo/notebook/DrawerActivity.java
+++ app/src/main/java/com/example/elilo/notebook/DrawerActivity.java
@@ -5,22 +5,38 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+import androidx.core.view.MenuItemCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class DrawerActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
 
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.navigation_view);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.colorAccent));
+        TextView textView = (TextView) findViewById(R.id.txtHeader);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.colorBlack));
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
@@ -29,5 +45,30 @@ public class DrawerActivity extends AppCompatActivity {
         DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
         ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, 0, 0);
         drawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Initialize menu inflater
+        MenuInflater menuInflater = getMenuInflater();
+        //Inflate menu
+        menuInflater.inflate(R.menu.menu_search,menu);
+        //Initialize menu item
+        MenuItem menuItem = menu.findItem(R.id.search_view);
+        //Initialize search view
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 }
